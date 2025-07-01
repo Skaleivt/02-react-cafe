4095ad2d2f9a1619eb2a5c2828209b767994d6ca
Skaleivt <skaleiviktor@gmail.com>
2025-07-02 00:54:03 +0300

    finish

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,8 +1,8 @@
 import css from "./VoteOptions.module.css";
-import type { VoteTypes } from "../../types/votes";
+import type { VoteType } from "../../types/votes";
 
 interface VoteOptionsProps {
-  onVote: (type: VoteTypes) => void;
+  onVote: (type: VoteType) => void;
   onReset: () => void;
   canReset: boolean;
 }
