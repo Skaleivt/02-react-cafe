5abbb5b3f77f2519b7d76d8e48668dcc09cc0fa4
Skaleivt <skaleiviktor@gmail.com>
2025-07-02 00:53:26 +0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
 import CafeInfo from "./components/CafeInfo/CafeInfo.tsx";
-import VoteOptions from "./components/VofeOptions/VoteOptions.tsx";
+import VoteOptions from "./components/VoteOptions/VoteOptions.tsx";
+import VoteStats from "./components/VoteStats/VoteStats.tsx";
+import Notification from "./components/Notification/Notification.tsx";
 import { useState } from "react";
 
-import type { Votes, VoteTypes } from "./types/votes.ts";
-
-interface Votes {
-  good: number;
-  neutral: number;
-  bad: number;
-}
+import type { Votes, VoteType } from "./types/votes.ts";
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
@@ -32,11 +28,28 @@ export default function App() {
       bad: 0,
     });
   }
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
 
   return (
     <div className="app">
       <CafeInfo />
-      <VoteOptions />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 }
